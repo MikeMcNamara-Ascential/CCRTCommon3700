e1cde10057b518630715f2fd40e68985074a6b06
tkelch <tkelch@80089e14-fa6c-c64e-badd-1f0333e23b03>
2012-06-09 20:32:01 +0000

    DEVELOPMENT. NOT TESTED. Added new KwpWinCcrtProtocolFilter to the protocol filter list.
    git-svn-id: http://10.105.1.46/svn/CCRT/Ccrt/branches/Common3700@13762 80089e14-fa6c-c64e-badd-1f0333e23b03

---
+++ Source/Core/ApplicationLayer/ProtocolFilters/Include/KwpWinCcrtProtocolFilter.h
@@ -0,0 +1,89 @@
+//*************************************************************************
+// Description:
+//   Interface class used to communicate with WinCcrtInterface
+//
+//===========================================================================
+// Copyright (c) 2003 Burke E. Porter Machinery
+// All Rights Reserved
+//
+// This file contains confidential information of Burke E. Porter Machinery
+// and is not to be used in any way detrimental to the interests thereof.
+// Unauthorized use, distribution, copying, or transmittal of this file in
+// any way is strictly prohibited.
+//===========================================================================
+//
+// LOG:
+//    $Log: /Core/ServicesLayer/Include/WinCcrtInterface.h $
+// 
+// 1     6/7/12 3:49p Tkelch
+// Initial creation and development of interface file
+//
+//******************************************************************************
+// $NoKeywords: $
+//==============================================================================
+
+#ifndef KwpWinCcrtProtocolFilter_h
+#define KwpWinCcrtProtocolFilter_h
+
+#include "ProtocolFilter.h"
+
+class KwpWinCcrtProtocolFilter : public KwpCanProtocolFilter
+{
+    public:
+      /**
+      * Class constructor.
+      *
+      * @param lastTxTime Variable used to hold the time of the last transmit operation. This is
+      *                   designed to be a shared timestamp that all the protocol filters for
+      *                   this module interface use so the module keep alive knows how long
+      *                   it has been since the last string was sent to the module so as not to
+      *                   send unnecessary keep alive messages
+      *
+      * @param commsInUse Semaphore to allow only one thread in the process to utilize the comm port at a time.
+      */
+      KwpWinCcrtProtocolFilter(KeepAliveTimer_t &lastTxTime, 
+                         StopCommsBepCondVar *stopCommsBepCondVar, 
+                         BepMutex *commPortInUse = NULL);
+      /**
+      * Class destructor.
+      */
+      virtual ~KwpWinCcrtProtocolFilter();
+      
+      /**
+      * Initialize the serial channel.
+      *
+      * @param config The configuration to use for the channel initialization.
+      * @return true if the initialization is successful.
+      */
+      virtual bool Initialize(std::string server);
+      
+      /**
+      * Send a message to a vehicle module.
+      * NOTE:  The module ID will be prepended to the message
+      *        prior to sending the message out of the port.
+      *
+      * @param message Message to be sent.
+      * 
+      * @return Status of the send.
+      */
+      virtual const BEP_STATUS_TYPE SendMessage(std::string a, std::string b);
+      
+protected:
+      /**
+      * Attempts to read an entire message from the Comm Proxy.
+      * If no data is returned in the alloted time, the 
+      * read will timeout and this method will return.
+      * 
+      * @param response Buffer to read the response into
+      * 
+      * @return Number of characters received or -1 on error
+      */
+      virtual int WaitForFullResponse( SerialString_t &response);  
+   
+private:
+      
+      IBepCommunication m_WinCcrtComm;
+};
+
+#endif //KwpWinCcrtProtocolFilter_h
+      
---
+++ Source/Core/ApplicationLayer/ProtocolFilters/KeyWord2000/KwpWinCcrtProtocolFilter.cpp
@@ -0,0 +1,70 @@
+//*************************************************************************
+// FILE:
+//    $Header: Core\ApplicationLayer\ProtocolFilters\KeyWord2000\KwpWinCcrtProtocolFilter.cpp 1     6/7/12 3:49p Tkelch $
+//
+// FILE DESCRIPTION:
+//   Interface class used to communicate with WinCcrtInterface
+//
+//===========================================================================
+// Copyright (c) 2003 Burke E. Porter Machinery
+// All Rights Reserved
+//
+// This file contains confidential information of Burke E. Porter Machinery
+// and is not to be used in any way detrimental to the interests thereof.
+// Unauthorized use, distribution, copying, or transmittal of this file in
+// any way is strictly prohibited.
+//===========================================================================
+//
+// LOG:
+//    $Log: /Core/ServicesLayer/Library/IWinInterfaceServer.cpp $
+//
+// 1     6/7/12 3:49p Tkelch
+// Initial creation and development of interface file
+//
+//*************************************************************************
+
+#include "KwpWinCcrtProtocolFilter.h"
+
+KwpWinCcrtProtocolFilter::KwpWinCcrtProtocolFilter(KeepAliveTimer_t &lastTxTime, StopCommsBepCondVar *stopCommsBepCondVar, BepMutex *commPortInUse) : KwpCanProtocolFilter(lastTxTime, stopCommsBepCondVar, commPortInUse)
+{
+}
+
+KwpWinCcrtProtocolFilter::~KwpWinCcrtProtocolFilter()
+{
+}
+
+KwpWinCcrtProtocolFilter::Initialize(std::string server)
+{
+   m_WinCcrtComm.Initialize(server);
+}
+
+const BEP_STATUS_TYPE KwpWinCcrtProtocolFilter::SendMessage(std::string messageTag, std::string msgValue, SerialString_t &response)
+{	// Add the module ID to the message
+	BEP_STATUS_TYPE status = BEP_STATUS_ERROR;
+   
+	Log(LOG_DETAILED_DATA, "KwpWinCcrtProtocolFilter::SendMessage - Resetting The Connection\n");
+	ResetConnection();
+	Log(LOG_DETAILED_DATA, "KwpWinCcrtProtocolFilter::SendMessage - Reset The Connection\n");
+	
+   // Send the message and return the result
+   status = m_WinCcrtComm.Write(messageTag, msgValue, response, true);
+   
+	// Return the status
+	return(status);
+}
+
+int KwpWinCcrtProtocolFilter::WaitForFullResponse(SerialString_t &response)
+{
+	int         retVal = 0;
+	// set up the receive timeout
+	Log(LOG_DEV_DATA, "KwpWinCcrtProtocolFilter::WaitForFullResponse - Waiting for response from module\n");
+	Log(LOG_DEV_DATA, "\t -- Module Response Delay: %d\n", GetResponseDelay());
+	Log(LOG_DEV_DATA, "\t -- Response End Delay: %d\n", GetResponseEndDelay());
+	
+   retVal = m_WinCcrtComm.Read(response, GetResponseDelay(), GetResponseEndDelay());
+   
+	Log(LOG_DEV_DATA, "Done waiting for response.  %d bytes received\n", retVal);
+	return( retVal);
+}
+
+

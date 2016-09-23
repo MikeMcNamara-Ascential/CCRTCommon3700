d0af04f5a19669f38e1906a21da85135c1ef835b
lpotts <lpotts@80089e14-fa6c-c64e-badd-1f0333e23b03>
2016-09-23 13:25:04 +0000

    Added code to account for someone walking in front of vehicle sensors.
    git-svn-id: http://10.105.1.46/svn/CCRT/Ccrt/branches/Common3700@24458 80089e14-fa6c-c64e-badd-1f0333e23b03

--- Source/Isuzu/ServicesLayer/Servers/SideSlipMonitor/SideSlipMonitor.cpp
+++ Source/Isuzu/ServicesLayer/Servers/SideSlipMonitor/SideSlipMonitor.cpp
@@ -31,6 +31,7 @@ SideSlipMonitor::~SideSlipMonitor()
 const INT32 SideSlipMonitor::HandlePulse(const INT32 code, const INT32 value)
 {
 	INT32 status = BEP_STATUS_ERROR;
+    Log(LOG_DEV_DATA, "Recieved code: %d, Recieved value: %d, axle count: %.1f", code, value, m_axleCount);
 	switch(code)
 	{
 	case SIDE_SLIP_MONITOR_PULSE_CODE:
@@ -41,7 +42,7 @@ const INT32 SideSlipMonitor::HandlePulse(const INT32 code, const INT32 value)
 			break;
 
 		case INTERAXLE_GAP_PULSE:
-			m_axleGapTimer.Stop();
+            m_axleGapTimer.Stop();
 			if(m_axleCount == 2.0)
 			{
 				char buff[8];
@@ -49,6 +50,7 @@ const INT32 SideSlipMonitor::HandlePulse(const INT32 code, const INT32 value)
 				status = m_ndb->Write("SideSlipValue", 
 									  CreateMessage(buff, sizeof(buff), "%.2f", m_currentMaxSideSlipValue), 
 									  response, true);
+                Log(LOG_DEV_DATA, "SideSlip value set to: %2f", m_currentMaxSideSlipValue);
 			}
 			else
 			{
@@ -64,10 +66,6 @@ const INT32 SideSlipMonitor::HandlePulse(const INT32 code, const INT32 value)
 			Log(LOG_DEV_DATA, "Axle time limit expired, stopping side slip timer (axle count: %.1f)", m_axleCount);
 			m_axleTimeLimitTimer.Stop();
 			m_sideSlipTimer.Stop();
-			if(m_axleCount == 0.5)
-			{
-				m_axleCount = 0.0;
-			}
 			break;
 
 		default:
@@ -92,12 +90,12 @@ void SideSlipMonitor::Initialize(const XmlNode *document)
 	// Get the conversion factor for the side slip data value
 	try
 	{
-		m_conversionFactor = BposReadInt(document->getChild("Setup/Parameters/SideSlipConversionFactor")->getValue().c_str());
+		m_conversionFactor = atof(document->getChild("Setup/Parameters/SideSlipConversionFactor")->getValue().c_str());
 	}
 	catch(XmlException &excpt)
 	{
-		Log(LOG_ERRORS, "Could not find conversion factor, setting to 1.0 - %s", excpt.GetReason());
-		m_conversionFactor = 1.0;
+		Log(LOG_ERRORS, "Could not find conversion factor, setting to 0.01 - %s", excpt.GetReason());
+		m_conversionFactor = 0.01;
 	}
 
 	// Setup the side slip timer
@@ -161,26 +159,37 @@ const string SideSlipMonitor::Publish(const XmlNode *node)
 	string result = BEP_ERROR_RESPONSE;
 	if(!node->getName().compare(GetDataTag("SideSlipIn")) && atob(node->getValue().c_str()))
 	{
-		Log(LOG_DEV_DATA, "In signal received - axle count = %.1f", m_axleCount);
-		if(m_axleCount == 0.0)
+        Log(LOG_DEV_DATA, "axleCount: %.1f", m_axleCount);
+        if (!(m_axleCount == 0.0 || m_axleCount == 1.0))
+        { //should not have axle enter before previous axle leaves, so must be whole number
+            m_axleCount = 0.0;                             //makes sure does not trigger if someone walks in front of vehicle sensor
+        }
+        if(m_axleCount == 0.0)
 		{
-			Log(LOG_DEV_DATA, "Vehicle front axle arrived at side slip tester, starting timer");
+			Log(LOG_DEV_DATA, "Vehicle front axle arrived at side slip tester, starting timer, reset currentMax");
 			m_sideSlipTimer.Start();
 			m_axleTimeLimitTimer.Start();
+            m_currentMaxSideSlipValue = 0.0;
 		}
-		m_axleCount += 0.5;
+        m_axleCount += 0.5;
 	}
 	else if(!node->getName().compare(GetDataTag("SideSlipOut")) && atob(node->getValue().c_str()))
 	{
 		m_sideSlipTimer.Stop();
 		m_axleTimeLimitTimer.Stop();
-		m_axleCount += 0.5;
+		if (m_axleCount != 0.0 && m_axleCount != 1.0) //vehicleOut should not start the axleCount, must be halfway b/t axles 
+        {                                             //makese sure does not trigger if someone walks in front of vehicle sensor
+            m_axleCount += 0.5;
+        }
+        else
+        {
+            m_axleCount = 0.0; //reset back to 0.0
+        }
 		if(m_axleCount == 1.0)
 		{
-			Log(LOG_DEV_DATA, "Starting axle gap timer");
 			m_axleGapTimer.Start();
 		}
-		Log(LOG_DEV_DATA, "Vehicle departed side slip tester, stopped timer, new axle count = %.1f", m_axleCount);
+		Log(LOG_DEV_DATA, "Vehicle departed side slip tester, stopped timer; axleCount: %.1f", m_axleCount);
 	}
 	result = BepServer::Publish(node);
 	return result;
@@ -189,7 +198,8 @@ const string SideSlipMonitor::Publish(const XmlNode *node)
 //-------------------------------------------------------------------------------------------------
 INT32 SideSlipMonitor::UpdateSideSlipValue()
 {
-	INT32 status = BEP_STATUS_ERROR;
+    Log(LOG_DEV_DATA, "entered updateSideSlipValueFunc");
+    INT32 status = BEP_STATUS_ERROR;
 	float currentSideSlipValue = -99.9;
 	string response;
 	string tag("RawSideSlipValue");
@@ -197,12 +207,19 @@ INT32 SideSlipMonitor::UpdateSideSlipValue()
 	status = m_ndb->Read(tag, response, true);
 	if(BEP_STATUS_SUCCESS == status)
 	{
-		status = m_ndb->GetByTag(tag, value, response);
+        //Log(LOG_DEV_DATA, "entered BEP_STATUS_SUCCESS 1");
+        status = m_ndb->GetByTag(tag, value, response);
 		if(BEP_STATUS_SUCCESS == status)
 		{
-			currentSideSlipValue = (float)BposReadInt(value.c_str()) * m_conversionFactor;
-			if(currentSideSlipValue > m_currentMaxSideSlipValue)
+            Log(LOG_DEV_DATA, "entered BEP_STATUS_SUCCESS 2, currentSideSlip: %f", currentSideSlipValue);
+            short temp = BposReadInt(value.c_str());
+            float fTemp = temp;
+            currentSideSlipValue = fTemp * m_conversionFactor;
+
+            Log(LOG_DEV_DATA, "currentSideSlip: %f, currentMax: %f, conversion: %f, value: %s, temp: %d, fTemp: %f", currentSideSlipValue, m_currentMaxSideSlipValue, m_conversionFactor, value.c_str(), temp, fTemp);
+			if(fabs(currentSideSlipValue) > fabs(m_currentMaxSideSlipValue)) //add in absValue (fabs for float) to this so the max slip is reported whether pos or neg
 			{
+                Log(LOG_DEV_DATA, "CurrentMax: %f, CurrentSideSlipVal: %f", m_currentMaxSideSlipValue, currentSideSlipValue);
 				m_currentMaxSideSlipValue = currentSideSlipValue;
 				Log(LOG_DEV_DATA, "New max side slip value: %.2f", m_currentMaxSideSlipValue);
 			}

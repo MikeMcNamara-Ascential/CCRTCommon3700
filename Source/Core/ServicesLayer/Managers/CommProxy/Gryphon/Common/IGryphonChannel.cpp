d5ce8c1b88de16fd480cf143fb4d4a10e17269ac
jsemann <jsemann@80089e14-fa6c-c64e-badd-1f0333e23b03>
2020-08-24 21:04:25 +0000

    changed j1939 address claim to server command (it is listed under server commands, even though documentation says sd_j1939)
    git-svn-id: http://10.105.1.46/svn/CCRT/Ccrt/branches/Common3700@29021 80089e14-fa6c-c64e-badd-1f0333e23b03

--- Source/Core/ServicesLayer/Managers/CommProxy/Gryphon/Common/IGryphonChannel.cpp
+++ Source/Core/ServicesLayer/Managers/CommProxy/Gryphon/Common/IGryphonChannel.cpp
@@ -1473,6 +1473,14 @@ BEP_STATUS_TYPE IGryphonChannel::processNewServerMsg(const uint8_t *inBuf, const
 					J1939TPEnableAck.Release();
 				}
 				break;
+			case CMD_J1939_ADDR_CLAIM:
+				Log( LOG_DETAILED_DATA, "Gryphon CMD_J1939_ADDR_CLAIM\n");
+				if(J1939AddressClaim.Acquire()== EOK)
+				{
+					J1939AddressClaim.Signal(true);
+					J1939AddressClaim.Release();
+				}
+				break;	// end of event names
 			default:
 				status = BEP_STATUS_HARDWARE;
 				Log(LOG_ERRORS, "Unknown response frame type [%02X] returned from Gryphon box!", inBuf[8]);
@@ -2536,7 +2544,7 @@ int IGryphonChannel::ClaimJ1939Address(UINT8 addressToClaim)
 			locStr[0], locStr[1], locStr[2], locStr[3],
 			locStr[4]);
 		Log(LOG_DETAILED_DATA, "Sending raw frame - locSize = %d", locSize);
-		retVal = SendRawFrame(locStr, locSize, FT_CMD, SD_J1939TP, true, J1939_ADDRESS_CLAIM_FC_MSG_SIZE);
+		retVal = SendRawFrame(locStr, locSize, FT_CMD, SD_SERVER, true, J1939_ADDRESS_CLAIM_FC_MSG_SIZE);
 		if(J1939AddressClaim.Wait(true,mSEC_nSEC(5000)) == EOK)
 		{
 			Log(LOG_DETAILED_DATA, "J1939 claimed address for tester %d", addressToClaim);
